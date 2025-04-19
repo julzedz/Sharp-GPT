4fb9ee704b4637255cfcad47ff095e1561a4715d
julzedz <edoziechukwuma@gmail.com>
2025-04-19 21:27:40 +0100

    Initialize frontend with Vite and React setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        
+      </div>
+      <p className=""></p>
+    </>
+  )
+}
+
+export default App
